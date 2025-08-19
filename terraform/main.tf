149c868a7144c0766f87d863338c77358a2857d4
hermenegildo.wiliamo <williamhermenegildo7@gmail.com>
2025-08-20 01:47:05 +0200

    setup da pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c4fc5dcabc9df21d"
+  ami           = "ami-07b36ea9852e986ad"
   instance_type = "t3.micro"
   key_name      = "analyzer-api-key"
 
