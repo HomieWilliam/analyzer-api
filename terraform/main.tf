eb09cd311d4430c4d418da461306b89a39891629
hermenegildo.wiliamo <williamhermenegildo7@gmail.com>
2025-08-20 01:54:43 +0200

    Setup da pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,17 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-07b36ea9852e986ad"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = "analyzer-api-key"
 
