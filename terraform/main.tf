fe4eb060a965369e9ca4084a4842f52af4e8ee1b
hermenegildo.wiliamo <williamhermenegildo7@gmail.com>
2025-08-20 03:00:57 +0200

    Setup da pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,19 @@
-variable "instance_types" {
-  type    = list(string)
-  default = ["t3.nano", "t3.micro", "t2.micro"]  # lista de tipos aceitáveis
+provider "aws" {
+  region = "eu-north-1"
 }
 
-# Data source para pegar a primeira instância disponível
-data "aws_instance_type_offerings" "available" {
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
   filter {
-    name   = "instance-type"
-    values = var.instance_types
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
-  location_type = "region"
 }
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = data.aws_instance_type_offerings.available.instance_types[0]
+  instance_type = "t3.nano"  # Tipo mínimo para não ultrapassar limite de vCPUs
   key_name      = "analyzer-api-key"
 
   tags = {
